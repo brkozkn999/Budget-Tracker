64a39378f7675c638c363ab0d955c1e5aa54b187
brkozkn999 <ibrahimberkozkan34@gmail.com>
2024-05-20 19:02:29 +0300

    name changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { Toaster } from "@/components/ui/sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Budget Tracker",
+  title: "Budget Buddy",
   description: "made by brkozkn999",
 };
 
